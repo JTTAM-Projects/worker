c40adf3107a27a05346d12585a40df6e4e678f7b
Leinonen96 <bhm506@myy.haaga-helia.fi>
2025-09-18 22:00:04 +0300

    refactor: Update .gitignore and remove tracked files

---
+++ backend/glig/src/main/java/com/jttam/glig/domain/employerprofile/EmployerProfileRepository.java
@@ -0,0 +1,8 @@
+package com.jttam.glig.domain.employerprofile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployerProfileRepository extends JpaRepository<EmployerProfile, Long> {
+}
