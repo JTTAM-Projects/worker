6fea8f7079109cd10dacaac9b4fc37f17bd18a27
tuojaakkola <bhm564@myy.haaga-helia.fi>
2025-09-30 11:12:46 +0300

    Push for Pull Reguest

--- frontend/workerfrontend/src/features/layoutcomponents/RootLayout.tsx
+++ frontend/workerfrontend/src/features/layoutcomponents/RootLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-
+// import Sidebar from "./Sidebar";
 export default function RootLayout() {
   return (
     <div className="bg-gray-50 min-h-screen w-full">
