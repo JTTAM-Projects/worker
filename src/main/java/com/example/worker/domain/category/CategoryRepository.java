944c0b382e10b265ae2c611d9444239c5a5f2245
Jani Könönen <160132682+janikononen@users.noreply.github.com>
2025-10-10 14:43:04 +0300

    Delete src directory

--- src/main/java/com/example/worker/domain/category/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.worker.domain.category;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
