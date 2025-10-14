3e86103c11ee679f3cc45140f210dcb59a721f50
TuomasJaakkola <emailaddress.com>
2025-10-14 19:51:13 +0300

    Updated navbar to use HeadlessUI's Menu component. Created empty pages for future development and routes to them. Also added some UI uppgrades.

--- frontend/workerfrontend/src/App.tsx
+++ frontend/workerfrontend/src/App.tsx
@@ -2,6 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ProfilePage from "./pages/ProfilePage";
 import TaskPage from "./pages/TaskPage";
+import CreateTaskPage from "./pages/CreateTaskPage";
+import MyTasksPage from "./pages/MyTasksPage";
+import ActiveApplicationsPage from "./pages/ActiveApplicationsPage";
+import AcceptedApplicationsPage from "./pages/AcceptedApplicationsPage";
+import PastApplicationsPage from "./pages/PastApplicationsPage";
 import RootLayout from "./features/layoutcomponents/RootLayout";
 
 export default function App() {
@@ -11,6 +16,17 @@ export default function App() {
         <Route index element={<LandingPage />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="tasks" element={<TaskPage />} />
+        <Route path="create-task" element={<CreateTaskPage />} />
+        <Route path="my-tasks" element={<MyTasksPage />} />
+        <Route
+          path="active-applications"
+          element={<ActiveApplicationsPage />}
+        />
+        <Route
+          path="accepted-applications"
+          element={<AcceptedApplicationsPage />}
+        />
+        <Route path="past-applications" element={<PastApplicationsPage />} />
       </Route>
     </Routes>
   );
