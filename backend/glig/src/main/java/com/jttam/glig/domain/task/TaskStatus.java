fed38a3288079e3069a9e24ffde6a85f9627980a
JaniKononen <bhm556@myy.haaga-helia.fi>
2025-11-17 21:30:04 +0200

    Added endpoint that returns all tasks for worker where he has an accepted application. It has same filtering possibilities that other endpoints are having. Updated swagger documentation for taskController. Added endpoint with validations that allows user to update task status. Added more enums to task status.

--- backend/glig/src/main/java/com/jttam/glig/domain/task/TaskStatus.java
+++ backend/glig/src/main/java/com/jttam/glig/domain/task/TaskStatus.java
@@ -1,11 +1,12 @@
 package com.jttam.glig.domain.task;
 
 public enum TaskStatus {
-    ACTIVE("Active"),
-    IN_PROGRESS("In progress"),
-    COMPLETED("Completed"),
-    CANCELLED("Cancelled"),
-    EXPIRED("Expired");
+    ACTIVE("Task that is waiting for applications from workers"),
+    IN_PROGRESS("Employer has accepted workers application"),
+    PENDING_APPROVAL("Pending employers approval"),
+    COMPLETED("Completed task that employer has accepted"),
+    CANCELLED("Employer or worker has cancelled task"),
+    EXPIRED("Expired due to time limit etc.");
 
     private final String displayName;
 
