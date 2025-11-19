bcc80eac067e0289873afa323d8dae920588d21b
JaniKononen <bhm556@myy.haaga-helia.fi>
2025-11-19 23:13:09 +0200

    added routes to app -folder. Added nativewind support to project and configured it. Builded tab navigation for pages.

---
+++ apps/mobiili/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { Tabs } from "expo-router";
+import "../../../global.css";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { ProfileButton } from "@/src/features/profile/ProfileButton";
+import { Entypo, FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function RootLayout() {
+  return (
+    <React.Fragment>
+      <StatusBar style="auto" />
+      <Tabs
+        screenOptions={{
+          headerRight: () => <ProfileButton />,
+          tabBarActiveTintColor: "#22c55e",
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Etusivu",
+            tabBarIcon: ({ color, size }) => <Entypo name="home" size={size} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="tasks"
+          options={{
+            title: "Työilmoitukset",
+            tabBarIcon: ({ color, size }) => <FontAwesome name="tasks" size={size} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="applications"
+          options={{
+            title: "Hakemukset",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="application-edit-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </React.Fragment>
+  );
+}
---
+++ apps/mobiili/src/app/(tabs)/tasks/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function tasks() {
+  return (
+    <View className="flex-1 items-center justify-center p-4">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
