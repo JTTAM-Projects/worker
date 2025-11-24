f51c3a053b790f1a060323b217883b0ffe38ea54
JaniKononen <bhm556@myy.haaga-helia.fi>
2025-11-24 15:12:37 +0200

    deleted all about changing application status ´completed´, endpoint, service method and updating all enums for application status

--- backend/glig/src/main/java/com/jttam/glig/domain/application/ApplicationStatus.java
+++ backend/glig/src/main/java/com/jttam/glig/domain/application/ApplicationStatus.java
@@ -1,13 +1,10 @@
 package com.jttam.glig.domain.application;
 
 public enum ApplicationStatus {
-    PENDING("Application pending"),
-    ACCEPTED("Application accepted"),
-    REJECTED("Application rejected"),
-    CANCELLED("Worker canceled apply"),
-    IN_PROGRESS("Application in progress"),
-    COMPLETED("Application completed"),
-    CONFIRMED("Application confirmed");
+    PENDING("Application pending and waiting accept from employer"),
+    ACCEPTED("Application accepted by employer"),
+    REJECTED("Application rejected by employer"),
+    CANCELLED("Worker canceled apply");
 
     private final String displayName;
 
